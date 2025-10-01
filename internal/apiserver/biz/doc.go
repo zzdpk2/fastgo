6ec4a923655bbb7cf5690b69064e45db228d3250
rex <zzdpk2@126.com>
2025-10-01 14:16:55 +0000

    chore: remove unused files

--- internal/apiserver/biz/doc.go
+++
@@ -1,8 +0,0 @@
-// Copyright 2024 孔令飞 <colin404@foxmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file. The original repo for
-// this file is https://github.com/onexstack/fastgo. The professional
-// version of this repository is https://github.com/onexstack/onex.
-
-// Package biz is the place where you can implements more complex business logic.
-package biz // import "github.com/onexstack/fastgo/internal/apiserver/biz"
--- internal/pkg/rid/doc.go
+++
@@ -1,7 +0,0 @@
-// Copyright 2024 孔令飞 <colin404@foxmail.com>. All rights reserved.
-// Use of this source code is governed by a MIT style
-// license that can be found in the LICENSE file. The original repo for
-// this file is https://github.com/onexstack/fastgo. The professional
-// version of this repository is https://github.com/onexstack/onex.
-
-package rid // import "github.com/onexstack/fastgo/internal/pkg/rid"
