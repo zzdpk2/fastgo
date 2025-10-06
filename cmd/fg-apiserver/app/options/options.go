29d32768d74c92235956aa85c55f21b191657076
rex <zzdpk2@126.com>
2025-10-06 04:09:47 +0000

    feat: modifying the MySQLOption structure and refactor the project

--- cmd/fg-apiserver/app/options/options.go
+++ cmd/fg-apiserver/app/options/options.go
@@ -1,11 +1,5 @@
 package options
 
-import (
-	"fmt"
-	"net"
-	"strconv"
-)
-
 type ServerOptions struct {
 	MySQLOptions *MySQLOptions `json:"mysql" mapstructure:"mysql"`
 }
@@ -14,57 +8,4 @@ func NewServerOptions() *ServerOptions {
 	return &ServerOptions{
 		MySQLOptions: NewMySQLOptions(),
 	}
-}
-
-// Validate checks ServerOptions
-func (o *ServerOptions) Validate() error {
-
-	if o.MySQLOptions.Addr == "" {
-		return fmt.Errorf("MySQL server address cannot be empty")
-	}
-	// check the address is like host:port
-	host, portStr, err := net.SplitHostPort(o.MySQLOptions.Addr)
-	if err != nil {
-		return fmt.Errorf("Invalid MySQL address format '%s': %w", o.MySQLOptions.Addr, err)
-	}
-
-	port, err := strconv.Atoi(portStr)
-	if err != nil || port < 1 || port > 65535 {
-		return fmt.Errorf("Invalid MySQL port: %s", portStr)
-	}
-
-	if host == "" {
-		return fmt.Errorf("MySQL hostname cannot be empty")
-	}
-
-	if o.MySQLOptions.Username == "" {
-		return fmt.Errorf("MySQL username cannot be empty")
-	}
-
-	if o.MySQLOptions.Password == "" {
-		return fmt.Errorf("MySQL password cannot be empty")
-	}
-
-	if o.MySQLOptions.Database == "" {
-		return fmt.Errorf("MySQL database name cannot be empty")
-	}
-
-	// Check connection pool
-	if o.MySQLOptions.MaxIdleConnections <= 0 {
-		return fmt.Errorf("MySQL max idle connections must be greater than 0")
-	}
-
-	if o.MySQLOptions.MaxOpenConnections <= 0 {
-		return fmt.Errorf("MySQL max open connections must be greater than 0")
-	}
-
-	if o.MySQLOptions.MaxIdleConnections > o.MySQLOptions.MaxOpenConnections {
-		return fmt.Errorf("MySQL max idle connections cannot be greater than max open connections")
-	}
-
-	if o.MySQLOptions.MaxConnectionLifeTime <= 0 {
-		return fmt.Errorf("MySQL max connection lifetime must be greater than 0")
-	}
-
-	return nil
 }
--- pkg/options/mysql_options.go
+++ pkg/options/mysql_options.go
@@ -1,9 +1,14 @@
 package options
 
 import (
+	"fmt"
+	"net"
+	"strconv"
 	"time"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
 )
-import "gorm.io/gorm"
 
 // MySQLOptions defines options for mysql database
 type MySQLOptions struct {
@@ -27,6 +32,70 @@ func NewMySQLOptions() *MySQLOptions {
 		MaxOpenConnections:    100,
 		MaxConnectionLifeTime: time.Duration(10) * time.Second,
 	}
+}
+
+// Validate checks ServerOptions
+func (o *MySQLOptions) Validate() error {
+
+	if o.Addr == "" {
+		return fmt.Errorf("MySQL server address cannot be empty")
+	}
+	// check the address is like host:port
+	host, portStr, err := net.SplitHostPort(o.Addr)
+	if err != nil {
+		return fmt.Errorf("Invalid MySQL address format '%s': %w", o.Addr, err)
+	}
+
+	port, err := strconv.Atoi(portStr)
+	if err != nil || port < 1 || port > 65535 {
+		return fmt.Errorf("Invalid MySQL port: %s", portStr)
+	}
+
+	if host == "" {
+		return fmt.Errorf("MySQL hostname cannot be empty")
+	}
+
+	if o.Username == "" {
+		return fmt.Errorf("MySQL username cannot be empty")
+	}
+
+	if o.Password == "" {
+		return fmt.Errorf("MySQL password cannot be empty")
+	}
+
+	if o.Database == "" {
+		return fmt.Errorf("MySQL database name cannot be empty")
+	}
+
+	// Check connection pool
+	if o.MaxIdleConnections <= 0 {
+		return fmt.Errorf("MySQL max idle connections must be greater than 0")
+	}
+
+	if o.MaxOpenConnections <= 0 {
+		return fmt.Errorf("MySQL max open connections must be greater than 0")
+	}
+
+	if o.MaxIdleConnections > o.MaxOpenConnections {
+		return fmt.Errorf("MySQL max idle connections cannot be greater than max open connections")
+	}
+
+	if o.MaxConnectionLifeTime <= 0 {
+		return fmt.Errorf("MySQL max connection lifetime must be greater than 0")
+	}
+
+	return nil
+}
+
+// DSN returns DSN from MySQLOptions.
+func (o *MySQLOptions) DSN() string {
+	return fmt.Sprintf(`%s:%s@tcp(%s)/%s?charset=utf8&parseTime=%t&loc=%s`,
+		o.Username,
+		o.Password,
+		o.Addr,
+		o.Database,
+		true,
+		"Local")
 }
 
 // NewDB create mysql store with the given config.
